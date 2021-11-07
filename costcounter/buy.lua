651e4579c5deacfabc47aea942ee9b10ab929119
BomberPlayz <49914603+BomberPlayz@users.noreply.github.com>
2021-11-07 14:08:13 +0100

    Update buy.lua

--- costcounter/buy.lua
+++ costcounter/buy.lua
@@ -9,11 +9,23 @@ if(type(args[k]) == "nil") then break end
   --print(args[k])
   
   for i=1,tonumber(args[k]) do
-    --print(args[k+1])
+   -- print(args[k+1])
      --print("cc: "..cost.calc("computer_probe"))
+    if args[k+1] == "all" then
+      --print("asdsad")
+      for kk in pairs(cost.costDb) do
+        --print("kk is "..kk.." and vv is "..vv)
+       -- print(kk)
+        local fora = cost.calc(kk)
+        print(kk.." for "..fora)
+        c = c+fora
+      end
+    else
+    
     c = c+cost.calc(args[k+1])
+    end
   end
   --print("acalc")
   k = k+2
 end
-print(c*0.75)
+print("COST: "..c)
