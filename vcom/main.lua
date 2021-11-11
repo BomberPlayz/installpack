36da9919db476febe61e0d9af54a21a4139821e7
afonya2 <64413731+afonya2@users.noreply.github.com>
2021-11-11 20:46:51 +0100

    Update main.lua

--- vcom/main.lua
+++ vcom/main.lua
@@ -2,6 +2,7 @@ local comp = require("computer")
 local com = require("component")
 
 local created = {{address="T9ste",name="Test",doc={test="prints hello!"},work={test=function() print("hello") end}}}
+local removed = {}
 local vcom = {}
 
 local oproxy = com.proxy
@@ -10,6 +11,11 @@ com.proxy = function(address)
     for k,v in ipairs(created) do
         if v.address == address then
             return v.work
+        end
+    end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
         end
     end
     return oproxy(address)
@@ -30,7 +36,15 @@ com.list = function(filter)
     end
     local datad = olist(filter)
     for k,v in pairs(datad) do
-        res[k] = v
+        local per = true
+        for ka,va in ipairs(removed) do
+            if va.address == k then
+                per = false
+            end
+        end
+        if per == true then
+            res[k] = v
+        end
     end
     return setmetatable(res,{
     __call=function()
@@ -49,6 +63,11 @@ com.type = function(address)
             return v.name
         end
     end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
+        end
+    end
     return otype(address)
 end
 
@@ -61,6 +80,11 @@ com.doc = function(address, data)
             return v.doc[data]
         end
     end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
+        end
+    end
     return odoc(address, data)
 end
 
@@ -70,6 +94,11 @@ com.slot = function(address)
     for k,v in ipairs(created) do
         if v.address == address then
             return -1
+        end
+    end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
         end
     end
     return oslot(address)
@@ -89,13 +118,18 @@ com.methods = function(address)
             return mets
         end
     end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
+        end
+    end
     return omethods(address)
 end
 
 local oinvoke = com.invoke
 com.invoke = function(address, method, ...)
     checkArg(1,address,"string")
-  checkArg(2,method,"string")
+	checkArg(2,method,"string")
     for k,v in ipairs(created) do
         if v.address == address then
             if v.work[method] == nil then
@@ -104,15 +138,25 @@ com.invoke = function(address, method, ...)
             return v.work[method](...)
         end
     end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
+        end
+    end
     return oinvoke(address, method, ...)
 end
 
 local ofields = com.fields
 com.fields = function(address)
     checkArg(1,address,"string")
-  for k,v in ipairs(created) do
+	for k,v in ipairs(created) do
         if v.address == address then
             return {}
+        end
+    end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            return
         end
     end
     return ofields(address)
@@ -120,9 +164,9 @@ end
 
 vcom.register = function(address,name,doc,work)
     checkArg(1,address,"string")
-  checkArg(1,name,"string")
+	checkArg(1,name,"string")
     checkArg(1,doc,"table")
-  checkArg(1,work,"table")
+	checkArg(1,work,"table")
     if com.type(address) ~= nil then
         return false, "Component exists!"
     end
@@ -142,7 +186,7 @@ vcom.unregister = function(address)
     for k,v in ipairs(created) do
         if v.address == address then
             created[k] = nil
-            comp.pushSignal("component_removed",address,thetype)
+            comp.pushSignal("component_removed",address,v.name)
             return true
         end
     end
@@ -152,6 +196,49 @@ end
 vcom.list = function() 
     local ret = {}
     for k,v in ipairs(created) do
+        table.insert(ret,v)
+    end
+    return ret
+end
+
+vcom.removereal = function(address) 
+    checkArg(1,address,"string")
+    if com.type(address) == nil then
+        return false, "Component not exists!"
+    end
+    for k,v in ipairs(created) do
+        if v.address == address then
+            return false, "Component created by vcom!"
+        end
+    end
+    table.insert(removed,{address=address,name=com.type(address)})
+    comp.pushSignal("component_removed",address,com.type(address))
+    return true
+end
+
+vcom.unremovereal = function(address) 
+    checkArg(1,address,"string")
+    if com.type(address) ~= nil then
+        return false, "Component exists!"
+    end
+    for k,v in ipairs(created) do
+        if v.address == address then
+            return false, "Component created by vcom!"
+        end
+    end
+    for k,v in ipairs(removed) do
+        if v.address == address then
+            removed[k] = nil
+            comp.pushSignal("component_added",address,v.name)
+            return true
+        end
+    end
+    return false, "an error"
+end
+
+vcom.removedlist = function() 
+    local ret = {}
+    for k,v in ipairs(removed) do
         table.insert(ret,v)
     end
     return ret
